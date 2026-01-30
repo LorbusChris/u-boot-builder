15cd1f10623590dc0278bbf787cb196bedee28e4
Christian Glombek <c.glombek@cosa.systems>
2026-01-30 04:36:46 +0100

    Fix Fairphone 5 build to match Tauchgang
    - Add optional header_version parameter for mkbootimg
    - Remove DEVICE_TREE from make command (DTB is concatenated manually)
    - Update FP5 config to use qcom-phone.config with header_version 2

--- build.sh
+++ build.sh
@@ -4,12 +4,20 @@ set -uexo pipefail
 
 CONFIG="$1"
 DEVICE_TREE="$2"
+HEADER_VERSION="${3:-}"
 
 cd ..
+
 make $CONFIG
-make -j $(nproc) DEVICE_TREE="$DEVICE_TREE"
+
+make -j $(nproc)
 gzip u-boot-nodtb.bin -c > u-boot-nodtb.bin.gz
 cat u-boot-nodtb.bin.gz dts/upstream/src/arm64/$DEVICE_TREE.dtb > u-boot-dtb
-mkbootimg --kernel_offset '0x00008000' --pagesize '4096' --kernel u-boot-dtb -o u-boot.img
+
+MKBOOTIMG_ARGS="--kernel u-boot-dtb --pagesize 4096 --base 0x0 --kernel_offset 0x8000 -o u-boot.img"
+if [ -n "$HEADER_VERSION" ]; then
+    MKBOOTIMG_ARGS="$MKBOOTIMG_ARGS --header_version $HEADER_VERSION"
+fi
+mkbootimg $MKBOOTIMG_ARGS
 mkdir -p builder/output/
 cp u-boot.img builder/output/u-boot.img
